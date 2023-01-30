ca65152603d2b36eeea3c3afc5591f3350e06a79
sugeng20 <sugeng.wanantara@gmail.com>
2023-01-30 14:04:32 +0700

    Membuat Custom Bottom Navigation

---
+++ lib/pages/home/main_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget cartButton() {
+      return FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: secondaryColor,
+        child: Image.asset(
+          'assets/icon_cart.png',
+          width: 20,
+        ),
+      );
+    }
+
+    Widget customNavbar() {
+      return ClipRRect(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
+        child: BottomAppBar(
+          shape: CircularNotchedRectangle(),
+          notchMargin: 10,
+          clipBehavior: Clip.antiAlias,
+          child: BottomNavigationBar(
+              backgroundColor: backgroundColor4,
+              type: BottomNavigationBarType.fixed,
+              items: [
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icon_home.png',
+                      width: 21,
+                    ),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icon_chat.png',
+                      width: 20,
+                    ),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icon_wishlist.png',
+                      width: 20,
+                    ),
+                    label: ''),
+                BottomNavigationBarItem(
+                    icon: Image.asset(
+                      'assets/icon_profile.png',
+                      width: 18,
+                    ),
+                    label: '')
+              ]),
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: backgroundColor1,
+      floatingActionButton: cartButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: customNavbar(),
+      body: Center(
+        child: Text("Main Page"),
+      ),
+    );
+  }
+}
