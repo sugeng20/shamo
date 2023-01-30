636948cf2276448a6bdde04b5cc9ce5ef37b818e
sugeng20 <sugeng.wanantara@gmail.com>
2023-01-30 13:07:45 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:shamo/pages/sign_in_page.dart';
+import 'package:shamo/pages/sign_up_page.dart';
+import 'package:shamo/pages/splash_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      routes: {
+        '/': (context) => SplashPage(),
+        '/sign-in': (context) => SignInPage(),
+        '/sign-up': (context) => SignUpPage()
+      },
+    );
+  }
+}
