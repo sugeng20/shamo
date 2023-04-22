0443c8834bbcdf5a10289f7a2c4152132d6069c9
sugeng20 <sugeng.wanantara@gmail.com>
2023-04-23 04:10:37 +0700

    Home Page Bagian Kategori

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -6,11 +6,138 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text(
-        'Home Page',
-        style: primaryTextStyle,
-      ),
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(
+            top: defaultMargin, left: defaultMargin, right: defaultMargin),
+        child: Row(
+          children: [
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Hallo, Sugeng',
+                    style: primaryTextStyle.copyWith(
+                        fontSize: 24, fontWeight: semiBold),
+                  ),
+                  Text(
+                    '@sugengdaily',
+                    style: subtitleTextStyle.copyWith(fontSize: 16),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              width: 54,
+              height: 54,
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: AssetImage('assets/image_profile.png'))),
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget categories() {
+      return Container(
+        margin: EdgeInsets.only(top: defaultMargin),
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: Row(
+            children: [
+              SizedBox(
+                width: defaultMargin,
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    color: primaryColor),
+                child: Text(
+                  'All Shoes',
+                  style: primaryTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: subtitleColor),
+                    color: transparentColor),
+                child: Text(
+                  'Running',
+                  style: subtitleTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: subtitleColor),
+                    color: transparentColor),
+                child: Text(
+                  'Training',
+                  style: subtitleTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: subtitleColor),
+                    color: transparentColor),
+                child: Text(
+                  'Basketball',
+                  style: subtitleTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(
+                  horizontal: 12,
+                  vertical: 10,
+                ),
+                margin: EdgeInsets.only(right: 16),
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12),
+                    border: Border.all(color: subtitleColor),
+                    color: transparentColor),
+                child: Text(
+                  'Hiking',
+                  style: subtitleTextStyle.copyWith(
+                      fontSize: 13, fontWeight: medium),
+                ),
+              )
+            ],
+          ),
+        ),
+      );
+    }
+
+    return ListView(
+      children: [header(), categories()],
     );
   }
 }
